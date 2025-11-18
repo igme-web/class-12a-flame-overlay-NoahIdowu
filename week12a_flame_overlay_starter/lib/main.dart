078d27ae3d9b18aa1a8601506f6810251f468240
NoahIdowu <92756085+NoahIdowu@users.noreply.github.com>
2025-11-17 20:47:38 -0500

    Finished week 12

--- week12a_flame_overlay_starter/lib/main.dart
+++ week12a_flame_overlay_starter/lib/main.dart
@@ -1,14 +1,26 @@
 import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import 'game.dart';
+import 'overlay_title.dart';
+import 'overlay_main.dart';
+import 'overlay_pause.dart';
+import 'overlay_info.dart';
+import 'overlay_settings.dart';
+import 'game_provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Flame.device.fullScreen();
 
-  runApp(const MainApp());
+  runApp(
+    /*const MainApp()*/ ChangeNotifierProvider(
+      create: (context) => GameProvider(),
+      child: const MainApp(),
+    ),
+  );
 }
 
 class MainApp extends StatelessWidget {
@@ -19,8 +31,28 @@ class MainApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: GameWidget(
-          game: OverlayTutorial(context),
+        body: GameWidget.controlled(
+          gameFactory: () => OverlayTutorial(context)..paused = true,
+          overlayBuilderMap: {
+            "title": (context, game) {
+              return OverlayTitle(game: game);
+            },
+            "main": (context, game) {
+              return mainOverlay(context, game);
+            },
+            "pause": (context, game) {
+              return pauseOverlay(context, game);
+            },
+            'info': (context, game) {
+              return InfoOverlay(
+                game: game as OverlayTutorial,
+              ); // Cast required!
+            },
+            'settings': (context, game) {
+              return settingsOverlay(context, game);
+            },
+          },
+          initialActiveOverlays: const ['title'],
         ),
       ),
     );
